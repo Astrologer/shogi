9cca6627117594cbbcfd06cc67bd4e7975b4c545
Denis <Astrologer@users.noreply.github.com>
2018-12-22 16:46:14 +0300

    predefined titles

--- js/args.js
+++ js/args.js
@@ -9,4 +9,5 @@ function Args() {
     });
 
     this.sfen = this.rawArgs.sfen || "lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b";
+    this.title = this.rawArgs.title;
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+var titles = {
+    ts1: '1 move tsume',
+    ts3: '3 moves tsume',
+    ts5: '5 moves tsume',
+    ts7: '7 moves tsume',
+    ts9: '9 moves tsume',
+}
+
 function createShogiBoard(boardId) {
     var args = new Args();
     var board = new ShogiBoard("boardX");
@@ -6,5 +14,8 @@ function createShogiBoard(boardId) {
     board.initBoard();
     board.initFigures(args.sfen);
 
+    if ('title' in args) {
+        document.title = titles[args.title] || args.title;
+    }
     return board;
 }
